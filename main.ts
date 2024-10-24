b64dd9720ed2e7fdf9e0da1004fcf63608ab9615
UltimateQuack <flamejantepato77@gmail.com>
2024-10-24 16:59:35 -0300

    Added main.py (aka i added pythin "support")

--- main.ts
+++ main.ts
@@ -1,20 +1,21 @@
-input.onPinPressed(TouchPin.P0, function () {
+input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
     player.change(LedSpriteProperty.Y, 0 - speed)
 })
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     player.change(LedSpriteProperty.X, 0 - speed)
 })
-input.onPinPressed(TouchPin.P2, function () {
+input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
     player.change(LedSpriteProperty.Y, speed)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     player.change(LedSpriteProperty.X, speed)
 })
-let player: game.LedSprite = null
+let player : game.LedSprite = null
 let speed = 0
 speed = 1
 player = game.createSprite(0, 0)
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     basic.pause(1000)
     speed += 0.1
 })
