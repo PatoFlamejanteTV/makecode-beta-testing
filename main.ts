3a9da05b9b0de9cc901c3d01211c178fba16b63d
UltimateQuack <flamejantepato77@gmail.com>
2024-10-24 16:57:35 -0300

    a

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 input.onPinPressed(TouchPin.P0, function () {
-    player.change(LedSpriteProperty.Y, -speed) // yeah, it sometimes glitches and shows "0 - speed" instead of "-speed"
+    player.change(LedSpriteProperty.Y, 0 - speed)
 })
 input.onButtonPressed(Button.A, function () {
-    player.change(LedSpriteProperty.X, -speed) // yeah, it sometimes glitches and shows "0 - speed" instead of "-speed"
+    player.change(LedSpriteProperty.X, 0 - speed)
 })
 input.onPinPressed(TouchPin.P2, function () {
     player.change(LedSpriteProperty.Y, speed)
@@ -22,3 +22,7 @@ DigitalPin.P2
 let getpresskeylist = sfcGamepad.cachePressedKeyList(
 )
 player = game.createSprite(0, 0)
+basic.forever(function () {
+    basic.pause(1000)
+    speed += 0.1
+})
