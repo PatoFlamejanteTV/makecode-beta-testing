5e274b6fdd095658db86cfcd04586436a009dbe8
UltimateQuack <flamejantepato77@gmail.com>
2024-10-24 16:39:19 -0300

    Small changes

--- main.ts
+++ main.ts
@@ -1,4 +1,23 @@
-let player = game.createSprite(0, 0)
-basic.forever(function () {
-	
+input.onPinPressed(TouchPin.P0, function () {
+    player.change(LedSpriteProperty.Y, -1)
 })
+input.onButtonPressed(Button.A, function () {
+    player.change(LedSpriteProperty.X, -1)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    player.change(LedSpriteProperty.Y, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    player.change(LedSpriteProperty.X, 1)
+})
+let player: game.LedSprite = null
+let sfcGamepad: sfc.gamepad = null
+let getpresskeylist = sfcGamepad.cachePressedKeyList(
+)
+// i dont KNOW IF THIS WOKRS 💜
+sfcGamepad = sfc.setSFC(
+DigitalPin.P0,
+DigitalPin.P1,
+DigitalPin.P2
+)
+player = game.createSprite(0, 0)
