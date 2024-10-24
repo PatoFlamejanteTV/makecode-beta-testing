e0c567301c370828b8d9f91ff2f9144aa2377c89
UltimateQuack <flamejantepato77@gmail.com>
2024-10-24 17:03:25 -0300

    SFX + idk fvkjghferjkgherkhj

--- main.ts
+++ main.ts
@@ -1,21 +1,21 @@
-input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
+input.onPinPressed(TouchPin.P0, function () {
     player.change(LedSpriteProperty.Y, 0 - speed)
 })
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     player.change(LedSpriteProperty.X, 0 - speed)
 })
-input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
+input.onPinPressed(TouchPin.P2, function () {
     player.change(LedSpriteProperty.Y, speed)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     player.change(LedSpriteProperty.X, speed)
 })
-let player : game.LedSprite = null
+let player: game.LedSprite = null
 let speed = 0
 speed = 1
 player = game.createSprite(0, 0)
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     basic.pause(1000)
-    speed += 0.1
+    music.play(music.createSoundExpression(WaveShape.Square, 1540, 1, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
+    speed += 0.5
 })
